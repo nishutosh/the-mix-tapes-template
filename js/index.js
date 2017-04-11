f98cd7fa2a5399a941485c02996fbc2bcb5a178d
wd002 <nishutosh002@gmail.com>
2017-04-11 21:57:04 +0530

    add

---
+++ js/index.js
@@ -0,0 +1,12 @@
+$(document).ready(function(){
+                            
+    $("#tagbox").click(function(){
+        
+        $(".second").fadeIn("slow");
+      
+        
+        
+    })                        
+                            
+                            
+                            })
